149a5d6e6cddf3c097d077219509efb4672a79f3
sonofme <sonofme@myself.com>
2012-10-22 12:09:50 -0600

    added fix_perms.sh to the files to fix perm on in fix_perms.sh

--- fix_perms.sh
+++ fix_perms.sh
@@ -4,6 +4,7 @@ chmod 644 index.php
 chmod 644 MoveServos.py
 chmod 600 README.md
 chmod 700 run_tests.sh
+chmod 700 fix_perms.sh
 find test_cases/ -type d -exec chmod 700 {} \;
 find test_cases/ -type f -exec chmod 600 {} \;
 
